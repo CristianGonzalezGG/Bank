d2a3717a8d7cafdea912249a5d2de1e7418ed7f1
portatil Ubuntu <cristancho1107@gmail.com>
2025-03-12 18:34:50 -0500

    loan problem

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Client, Account
+from .models import Client, Account, Loan, Payment, Appointment, UserTwoFactorSettings, TwoFactorCode
 
-# Personalización del administrador para el modelo Client
-
-# Registra los modelos en el admin
+# Registrar los modelos en el admin
 admin.site.register(Client)
 admin.site.register(Account)
+admin.site.register(Loan)
+admin.site.register(Payment)
+admin.site.register(Appointment)
+admin.site.register(UserTwoFactorSettings)
+admin.site.register(TwoFactorCode)
